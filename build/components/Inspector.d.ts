3bcc2a5b1d643ee2edb307dbec37e84937ecb40d
Abdur-Rahman <69679506+ShafSpecs@users.noreply.github.com>
2022-08-16 16:37:09 +0100

    Delete build directory

--- build/components/Inspector.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-declare type POSITION = "top-right" | "bottom-left" | "top-left" | "bottom-right";
-export declare type RemixInspectorProps = {
-    defaultOpen?: boolean;
-    draggable?: boolean;
-    position?: POSITION;
-};
-export declare function RemixInspector({ defaultOpen, draggable, position }: RemixInspectorProps): JSX.Element;
-export {};
