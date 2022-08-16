d7f20ac29c720834815132334f50cb4c2257cd33
Abdur-Rahman <69679506+ShafSpecs@users.noreply.github.com>
2022-08-16 16:36:58 +0100

    Delete browser directory

--- browser/components/icons.d.ts
+++
@@ -1,4 +0,0 @@
-import type { SVGProps } from "react";
-export default function CodeIcon(props: SVGProps<SVGSVGElement>): JSX.Element;
-export declare function RefreshIcon(props: SVGProps<SVGSVGElement>): JSX.Element;
-export declare function CloseIcon(props: SVGProps<SVGSVGElement>): JSX.Element;
